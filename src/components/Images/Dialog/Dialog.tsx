9ed5569d15517e3e621bd4791f515d5b8dbe3a7b
sophia-ma <markoglou.sofia@gmail.com>
2020-10-10 16:16:45 +0100

    Used material dialog

--- src/components/Images/Dialog/Dialog.tsx
+++ src/components/Images/Dialog/Dialog.tsx
@@ -1,7 +1,6 @@
 import Img from 'gatsby-image';
 import React from 'react';
-import Dialog, { DialogContent } from '@reach/dialog';
-import '@reach/dialog/styles.css';
+import Dialog from '@material-ui/core/Dialog';
 import './Dialog.scss';
 
 type Props = {
@@ -14,18 +13,8 @@ const ImageDialog: React.FC<Props> = props => {
     const { close, selectedValue, open } = props;
 
     return (
-        <Dialog isOpen={open} onDismiss={close} aria-label="image-dialog">
-            <DialogContent
-                aria-label="image-dialog-content"
-                style={{
-                    position: `absolute`,
-                    left: `50%`,
-                    top: `50%`,
-                    transform: `translate(-50%, -50%)`,
-                }}
-            >
-                <Img fluid={selectedValue.node.fluid} />
-            </DialogContent>
+        <Dialog open={open} onClose={close} aria-labelledby="image-dialog" fullWidth={true}>
+            <Img fluid={selectedValue.node.fluid} />
         </Dialog>
     );
 };

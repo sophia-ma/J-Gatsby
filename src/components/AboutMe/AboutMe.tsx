c3714d736ef31c1fe9e4e3651e0e9ecb7988b5e9
sophia-ma <markoglou.sofia@gmail.com>
2020-11-18 23:54:29 +0000

    Added about me query

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import Divider from 'images/divider.png';
 import './AboutMe.scss';
 
+interface DataProps {
+    contentfulAboutMeTextRichTextNode: {
+        json: any;
+    };
+}
 const AboutMe: React.FC = () => {
+    const data: DataProps = useStaticQuery(graphql`
+        query {
+            contentfulAboutMeTextRichTextNode {
+                json
+            }
+        }
+    `);
+
     return (
         <div id="about-me" className="about-me-container">
             <h1 className="heading">About me</h1>
             <img src={Divider} alt="divider" className="divider" />
 
-            <p className="content">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent malesuada augue
-                neque, id hendrerit lacus accumsan id. Suspendisse potenti. Nam ut ligula at felis
-                molestie cursus consectetur et justo. Donec vitae elementum arcu. Suspendisse
-                potenti. Nunc fringilla, purus eget mattis luctus, metus purus lobortis leo, ac
-                ornare lorem lacus sed sapien. Vivamus vel efficitur eros, id semper augue. Nunc sit
-                amet mattis lectus, vitae tristique ligula. Praesent convallis eget orci ut mattis.
-                Nam imperdiet pharetra felis, ac iaculis nulla dapibus sed. Nulla dapibus vestibulum
-                quam sed molestie. Donec volutpat et dui sed fermentum.
-            </p>
+            <div className="content">
+                {documentToReactComponents(data.contentfulAboutMeTextRichTextNode.json)}
+            </div>
         </div>
     );
 };

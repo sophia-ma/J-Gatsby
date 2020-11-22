b2f7cc6c4540684e3058f7423dd3d65238aff280
sophia-ma <markoglou.sofia@gmail.com>
2020-11-22 19:14:21 +0000

    Added see more on about me section

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,28 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import Divider from 'images/divider.png';
 import './AboutMe.scss';
 
 interface DataProps {
-    contentfulAboutMeTextRichTextNode: {
-        json: any;
+    allContentfulSection: {
+        nodes: Node[];
+    };
+}
+
+interface Node {
+    aboutMe: {
+        aboutMe: string;
+    };
+    aboutMeSeeMore: {
+        aboutMeSeeMore: string;
     };
 }
 const AboutMe: React.FC = () => {
     const data: DataProps = useStaticQuery(graphql`
         query {
-            contentfulAboutMeTextRichTextNode {
-                json
+            allContentfulSection {
+                nodes {
+                    aboutMe {
+                        aboutMe
+                    }
+                    aboutMeSeeMore {
+                        aboutMeSeeMore
+                    }
+                }
             }
         }
     `);
 
-    const options = {
-        renderText: (text: string) => {
-            return text.split('\n').flatMap((text, i) => [<br />, text]);
-        },
+    const getRichText = (text: string) => {
+        return text.split('\n').flatMap((text, i) => [<br />, text]);
     };
+
+    const [moreContentStyle, setMoreContentStyle] = useState('none');
+    const [seeMoreStyle, setSeeMoreStyle] = useState('inline');
 
     return (
         <div id="about-me" className="about-me-container">
@@ -30,7 +46,21 @@ const AboutMe: React.FC = () => {
             <img src={Divider} alt="divider" className="divider" />
 
             <div className="content">
-                {documentToReactComponents(data.contentfulAboutMeTextRichTextNode.json, options)}
+                {getRichText(data.allContentfulSection.nodes[0].aboutMe.aboutMe)}{' '}
+                <span
+                    className="see-more"
+                    style={{ display: seeMoreStyle }}
+                    onClick={() => {
+                        setMoreContentStyle('block');
+                        setSeeMoreStyle('none');
+                    }}
+                >
+                    ...See more
+                </span>
+            </div>
+
+            <div className="content" style={{ display: moreContentStyle }}>
+                {getRichText(data.allContentfulSection.nodes[0].aboutMeSeeMore.aboutMeSeeMore)}
             </div>
         </div>
     );
